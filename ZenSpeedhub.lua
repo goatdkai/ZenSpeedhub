23cfdb4f1e573f5d66c7f8ea5d6c4befe414adb8
goatdkai <goatdkai@gmail.com>
2026-07-14 17:51:18 +0800

    Update ZenSpeedhub.lua

--- ZenSpeedhub.lua
+++ ZenSpeedhub.lua
@@ -1,1 +1,5 @@
+hereloadstring(game:HttpGet("https://raw.githubusercontent.com/goatdkai/ZenhubX/refs/heads/main/ZenXSpeedHub.lua"))()
 
+task.wait(0.5)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
